594834abad9206fa14426c846b427eafcafeef98
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2023-08-07 17:19:27 -0300

    Replicate ci pipeline locally with scripts
    Developer scripts (dev/test.sh and dev/fmt.sh) help developers to mimic
    ci behavior locally, to prevent failing on the production repo

---
+++ dev/fmt.sh
@@ -0,0 +1,12 @@
+#! /bin/env sh
+# This helper script formats the code according to black and isort standarts.
+set -e
+cd "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/../"
+
+echo '+ black (start)'
+poetry run black .
+echo '+ black (end)'
+
+echo '+ isort (start)'
+poetry run isort .
+echo '+ isort (end)'
---
+++ dev/test.sh
@@ -0,0 +1,24 @@
+#! /bin/env sh
+# This helper script runs all tests and checks included in the project's CI
+# pipeline, for easy local testing.
+
+set -e
+cd "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/../"
+
+echo "Python tests and checks:"
+
+echo "+ black (start)"
+poetry run black --check .
+echo "+ black (end)"
+
+echo "+ isort (start)"
+poetry run isort --check .
+echo "- isort (end)"
+
+echo "+ mypy (start)"
+poetry run mypy .
+echo "+ mypy (end)"
+
+echo "+ pytest (start)"
+poetry run pytest --doctest-modules -v
+echo "- pytest (end)"
