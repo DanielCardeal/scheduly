103e2896898b5a3fa009d9f7712d08c800451051
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2023-09-17 17:29:25 -0300

    Create and implement ASPData conversion protocol

---
+++ docs/model.md
@@ -0,0 +1,77 @@
+# Model
+
+This page lists and explains all of the predicates and values used within the project.
+
+## Values
+
++ course id (symbol | number): a symbol that **uniquely** identifies a course offered by the institute. 
+
+  > Example values: mac0110, mac5770, mae0217, 4310145
+
++ teacher id (symbol): a symbol that **uniquely** identifies one of the teachers of the department. 
+
+  > Example values: turing, ada_lovelace, gracehopper
+
++ day (number $\in [1,5]$): a day of the week.
+
+  > Example values: 1 (Monday), 2 (Tuesday), 5 (Friday)
+
++ period (number $\in [1,4]$): timeslots available for each day. In IME-USP, particularly:
+
+  + 1 => classes from 8 to 9:40 AM
+  + 2 => classes from 10 to 11:40 AM
+  + 3 => classes from 2 to 3:40 PM
+  + 4 => classes from 4 to 6:40 PM
+
+## Predicates
+
+
+### Courses' information
+
++ `course/5(course id, course group, number of weekly classes, ideal period, is double)`
+
+  General information about a course in the educational institution.  
+
++ `joint/2(course A id, course B id)`
+
+  Indicates that `course A` and `course B` should be offered in the same
+  classroom and at the same time.
+
+### Curricula information
+
++ `curriculum/2(curriculum id, curriculum group)`
+
+  Represents a curriculum in the institution. 
+
++ `curriculum_component/3(curriculum id, course id, is required)`
+
+  Assign a `course` as part of a `curriculum`. The component can be optionally
+  marked as 'required', indicating that a student must take this course to
+  complete the curriculum.
+
+### Teachers' information
+
++ `teacher/1(teacher id)`
+
+  Identifies a teacher in the scheduler
+
++ available/3(teacher id, weekday, period)
+
+  Indicates which day/period a teacher is available to teach.
+
++ preferred/3(teacher id, weekday, period)
+
+  Indicates preferred lecture day/period of a teacher
+
+### Offerings' information
+
++ `lecturer/3(course id, offering group, teacher id)`
+
+  Assign `teacher` as the lecturer of `course` in this given semester. Note that
+  there could be multiple instances for a same course/offering group, which
+  means more than one teacher is assigned to the same class.
+
++ `class/5(course id, offering group, teacher id, weekday, period)`
+
+  Schedule a class of a given course to a weekday
+
--- ime_usp_class_scheduler/model/data.py
+++ ime_usp_class_scheduler/model/data.py
@@ -1,6 +1,17 @@
 from __future__ import annotations
 
+from typing import Protocol
+
 from attrs import field, frozen, validators
+from clingo.symbol import Function, Number, String, Symbol
+
+
+class ASPData(Protocol):
+    """Protocol of all classes that can be converted to ASP"""
+
+    def to_asp(self) -> list[Symbol]:
+        """Convert instance of the object to its ASP symbol representation."""
+        ...
 
 
 @frozen
@@ -13,6 +24,21 @@ class CourseData:
     group: str = field(converter=str)
     ideal_period: int = field(validator=validators.instance_of(int))
 
+    def to_asp(self) -> list[Symbol]:
+        """Convert self into a course/5 ASP predicate."""
+        return [
+            Function(
+                "course",
+                [
+                    String(self.course_id),
+                    String(self.group),
+                    Number(self.num_classes),
+                    Number(self.ideal_period),
+                    Number(self.is_double),
+                ],
+            )
+        ]
+
 
 @frozen
 class TeacherData:
@@ -21,6 +47,24 @@ class TeacherData:
     teacher_id: str = field(converter=str)
     available_time: set[ScheduleTimeslot] = field(validator=validators.instance_of(set))
     preferred_time: set[ScheduleTimeslot] = field(validator=validators.instance_of(set))
+
+    def to_asp(self) -> list[Symbol]:
+        """Convert self into teacher/1, available/3 and preferred/3 ASP predicates."""
+        available = [
+            Function(
+                "available",
+                [String(self.teacher_id), Number(t.weekday), Number(t.period)],
+            )
+            for t in self.available_time
+        ]
+        preferred = [
+            Function(
+                "preferred",
+                [String(self.teacher_id), Number(t.weekday), Number(t.period)],
+            )
+            for t in self.preferred_time
+        ]
+        return available + preferred
 
 
 @frozen(order=True)
@@ -46,10 +90,37 @@ class WorkloadData:
     periods for the remaining classes.
     """
 
+    # TODO: implement multiple teachers for one class
     course_id: str = field(converter=str)
     teacher_id: str = field(converter=str)
-    group: str = field(converter=str)
+    offering_group: str = field(converter=str)
     fixed_classes: set[ScheduleTimeslot] = field(validator=validators.instance_of(set))
+
+    def to_asp(self) -> list[Symbol]:
+        """Convert self into lecturer/3 and class/5 ASP predicates."""
+        lectures = Function(
+            "lecturer",
+            [
+                String(self.course_id),
+                String(self.offering_group),
+                String(self.teacher_id),
+            ],
+        )
+        fixed = [
+            Function(
+                "class",
+                [
+                    String(self.course_id),
+                    String(self.offering_group),
+                    String(self.teacher_id),
+                    Number(t.weekday),
+                    Number(t.period),
+                ],
+            )
+            for t in self.fixed_classes
+        ]
+
+        return [lectures] + fixed
 
 
 @frozen
@@ -59,6 +130,10 @@ class JointClassData:
     course_id_A: str = field(converter=str)
     course_id_B: str = field(converter=str)
 
+    def to_asp(self) -> list[Symbol]:
+        "Convert self into a joint/2 ASP predicate."
+        return [Function("joint", [String(self.course_id_A), String(self.course_id_B)])]
+
 
 @frozen
 class CurriculaData:
@@ -67,6 +142,24 @@ class CurriculaData:
     curriculum_id: str = field(converter=str)
     group: str = field(converter=str)
     courses: set[CurriculaCoursesData] = field(validator=validators.instance_of(set))
+
+    def to_asp(self) -> list[Symbol]:
+        """Convert self into curriculum/3 and curriculum_component/3 ASP predicates."""
+        curriculum = Function(
+            "curriculum", [String(self.curriculum_id), String(self.group)]
+        )
+        curriculum_components = [
+            Function(
+                "curriculum_component",
+                [
+                    String(self.curriculum_id),
+                    String(data.course_id),
+                    Number(data.is_required),
+                ],
+            )
+            for data in self.courses
+        ]
+        return [curriculum] + curriculum_components
 
 
 @frozen
