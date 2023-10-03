221b76cce06cc34f818d31fc209c49cba8035c9b
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2023-10-03 15:48:54 -0300

    Create utility for creating constraints

--- src/py/ime_usp_class_scheduler/main.py
+++ src/py/ime_usp_class_scheduler/main.py
@@ -1,9 +1,11 @@
-import logging
 from pathlib import Path
+from textwrap import dedent
 from typing import Optional
 
 import click
+from rich.prompt import Confirm, Prompt
 
+from ime_usp_class_scheduler.constants import HARD_CONSTRAINTS_DIR, SOFT_CONSTRAINTS_DIR
 from ime_usp_class_scheduler.interface.CliInterface import CliInterface
 from ime_usp_class_scheduler.interface.configuration import load_preset
 from ime_usp_class_scheduler.logging import error, info
@@ -57,6 +59,60 @@ def cli(
         interface.save_model(model_out_path)
 
     interface.run()
+
+
+@main.command()
+@click.argument("constraint-type", type=click.Choice(["hard", "soft"]), nargs=1)
+def new(constraint_type: str):
+    assert constraint_type in (
+        "hard",
+        "soft",
+    ), f"Invalid constraint type: {constraint_type}"
+
+    name = Prompt.ask("Enter the constraint name")
+    if not name:
+        error("The constraint name cannot be empty.")
+        exit(1)
+    name = "_".join([word.lower() for word in name.split()])
+
+    description = Prompt.ask(
+        "Enter the constraint description",
+        default="Constraint description",
+        show_default=False,
+    )
+
+    if constraint_type == "hard":
+        path = HARD_CONSTRAINTS_DIR.joinpath(name).with_suffix(".lp")
+        contents = dedent(
+            f"""
+        %*
+        {description}
+        *%
+        :- % your rules here
+        """
+        )
+    elif constraint_type == "soft":
+        path = SOFT_CONSTRAINTS_DIR.joinpath(name).with_suffix(".lp")
+        contents = dedent(
+            f"""
+        %*
+        {description}
+        *%
+        :~ %* constraint logic *%.
+        [w_{name}@p_{name}, %* terms *% ]
+        """
+        )
+
+    if not path.exists() or Confirm.ask(
+        "There is already a soft constraint with this name. Override?"
+    ):
+        with open(path, "w") as f:
+            f.write(contents)
+        info(f"Created {path} with the following contents:")
+        # NOTE: Don't use rich because of soft constraint brackets
+        print(contents)
+    else:
+        print()
 
 
 if __name__ == "__main__":
