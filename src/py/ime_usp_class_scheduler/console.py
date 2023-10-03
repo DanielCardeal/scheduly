1137d781b8325589b427f9805334037b92d089f8
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2023-10-03 17:00:34 -0300

    Use console.print instead of console.log in logs

--- src/py/ime_usp_class_scheduler/console.py
+++ src/py/ime_usp_class_scheduler/console.py
@@ -8,17 +8,17 @@ err_console = Console(stderr=True)
 
 def log_info(message: str) -> None:
     """Logs an information to the standard output."""
-    console.log(f":info: [dim cyan]INFO[/] {message}")
+    console.print(f"[dim cyan]INFO[/] {message}")
 
 
 def log_warn(message: str) -> None:
     """Logs a warning to the standard output."""
-    console.log(f":warning: [dim yellow]WARN[/] {message}")
+    console.print(f"[dim yellow]WARN[/] {message}")
 
 
 def log_error(message: str) -> None:
     """Logs an error to the stderr."""
-    err_console.log(f":error: [dim red]ERROR[/] {message}")
+    err_console.print(f"[dim red]ERROR[/] {message}")
 
 
 def log_exception() -> None:
