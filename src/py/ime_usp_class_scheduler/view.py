a5005ac699d8bdae938c9ad36aa19ad06eda5550
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2023-10-27 16:52:25 -0300

    Update view for multiple jointed courses

--- src/py/ime_usp_class_scheduler/view.py
+++ src/py/ime_usp_class_scheduler/view.py
@@ -24,7 +24,7 @@ class ModelView(ABC):
 
     classes: set[ClassData]
     conflicts: set[ConflictData]
-    jointed: dict[tuple[str, str], str]
+    jointed: set[JointedData]
 
     def __init__(self) -> None:
         super().__init__()
@@ -47,7 +47,7 @@ class ModelView(ABC):
         self._clear_schedule()
         self.classes = set()
         self.conflicts = set()
-        self.jointed = dict()
+        self.jointed = set()
 
         for symbol in model.symbols:
             if symbol.type is not SymbolType.Function:
@@ -56,13 +56,7 @@ class ModelView(ABC):
                 case "class":
                     self.classes.add(ClassData.from_asp(symbol))
                 case "_joint":
-                    jointed_classes = JointedData.from_asp(symbol)
-                    self.jointed[
-                        (jointed_classes.course_id_a, jointed_classes.offering_group)
-                    ] = jointed_classes.course_id_b
-                    self.jointed[
-                        (jointed_classes.course_id_b, jointed_classes.offering_group)
-                    ] = jointed_classes.course_id_a
+                    self.jointed.add(JointedData.from_asp(symbol))
 
 
 class CliTabularView(ModelView):
@@ -71,10 +65,67 @@ class CliTabularView(ModelView):
     def _update_schedule(self, model: ModelResult) -> None:
         super()._update_schedule(model)
 
+        # Get a mapping from (course, offering) -> set of jointed classes
+        joint_mapping: dict[tuple[str, str], set[str]] = dict()
+        for j in self.jointed:
+            seen_a = (j.course_id_a, j.offering_group) in joint_mapping
+            seen_b = (j.course_id_b, j.offering_group) in joint_mapping
+            if not seen_a and not seen_b:
+                s = {j.course_id_a, j.course_id_b}
+                joint_mapping[(j.course_id_a, j.offering_group)] = s
+                joint_mapping[(j.course_id_b, j.offering_group)] = s
+            elif seen_a and not seen_b:
+                s = joint_mapping[(j.course_id_a, j.offering_group)]
+                joint_mapping[(j.course_id_b, j.offering_group)] = s
+                s.add(j.course_id_b)
+            elif not seen_a and not seen_b:
+                s = joint_mapping[(j.course_id_b, j.offering_group)]
+                joint_mapping[(j.course_id_a, j.offering_group)] = s
+                s.add(j.course_id_a)
+            elif seen_a and seen_b:
+                # Both exist: unify both the sets into one set
+                s1 = joint_mapping[(j.course_id_a, j.offering_group)]
+                s2 = joint_mapping[(j.course_id_b, j.offering_group)]
+                s1.update(s2)
+                for course_id in s2:
+                    joint_mapping[(course_id, j.offering_group)] = s1
+
+        # Fill the schedule, marking already added classes in the `courses` dict:
+        # courses[course_id, offering group, weekday, period] = was already added?
+        courses: dict[tuple[str, str, int, int], bool] = dict()
         for class_ in self.classes:
-            class_str = f"{class_.course_id} ({class_.offering_group[:MAX_OFFERING_GROUP_LENGTH]})"
-            if (class_.course_id, class_.offering_group) in self.jointed:
-                class_str += " *"
+            if (
+                class_.course_id,
+                class_.offering_group,
+                class_.weekday,
+                class_.period,
+            ) in courses:
+                # Ignores already added classes
+                continue
+
+            class_str: str
+            if (class_.course_id, class_.offering_group) in joint_mapping:
+                # Dealing with a jointed class
+                jointed_classes = joint_mapping[class_.course_id, class_.offering_group]
+                class_str = " - ".join(jointed_classes)
+                for course_id in jointed_classes:
+                    courses[
+                        course_id,
+                        class_.offering_group,
+                        class_.weekday,
+                        class_.period,
+                    ] = True
+            else:
+                # Non jointed classes
+                class_str = f"{class_.course_id}"
+                courses[
+                    class_.course_id,
+                    class_.offering_group,
+                    class_.weekday,
+                    class_.period,
+                ] = True
+
+            class_str += f" ({class_.offering_group[:MAX_OFFERING_GROUP_LENGTH]})"
             self.schedule[class_.weekday][class_.period].add(class_str)
 
     def show_model(self, model: ModelResult) -> None:
